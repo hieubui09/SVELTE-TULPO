b315e13a5000d3a3bb0b490cfa718c4499e07f35
hieubui09 <hieubdph09616@fpt.edu.vn>
2021-05-11 17:46:28 +0700

    11-5

--- src/Layout/Header.svelte
+++ src/Layout/Header.svelte
@@ -1,5 +1,9 @@
 <script>
-
+    	import { fade} from 'svelte/transition';
+  let mobileMenu = false;
+    function menuMobi() {
+        mobileMenu = !mobileMenu;
+    }
 </script>
 
 <header>
@@ -16,6 +20,24 @@
            <div class="menu__item dv"><a href="">Sản xuất</a></div>
            <div class="menu__item"><a href="">Sự nghiệp</a></div>
            <div class="menu__item"><a href="">Tuyển dụng</a></div>
+       </div>
+       <div class="icon__menu" on:click={menuMobi}>
+        {#if !mobileMenu}
+           <img out:fade in:fade src="images/cate.png" alt="">
+           {:else}
+            <p out:fade in:fade >&#10539;</p>       
+           {/if}
+       </div>
+       <div class="menu__mobi" class:menu__mobi--active={mobileMenu}>
+        <ul>
+            <li><a href="">Trang chủ</a></li>
+            <li><a href="#news">Dịch vụ</a></li>
+            <li><a href="#contact">Quy trình</a></li>
+            <li><a href="#about">Sản xuất</a></li>
+            <li><a href="#about">Sự nghiệp</a></li>
+            <li><a href="#about">Tuyển dụng</a></li>
+          </ul>
+          
        </div>
    </div>
 </header>
